fd5f09e2005006282a97ca16012d2439f48e7916
jinikuadmin <jinikuadmin@akcpwrxn1zh3qd.local>
2016-12-19 00:00:10 +0900

    Initial Commit

---
+++ Dentaku2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dentaku2
+//
+//  Created by jinikuadmin on 2016/12/11.
+//  Copyright © 2016年 jinikuadmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
