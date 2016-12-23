e340fcbe8f4f79dd6232a8eba9c9c2d3b21b531a
jinikuadmin <jinikuadmin@akcpwrxn1zh3qd.local>
2016-12-23 16:36:09 +0900

    12/23成功確認

--- Dentaku2/ViewController.swift
+++ Dentaku2/ViewController.swift
@@ -11,37 +11,40 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var resultTextView: UITextView!
-    
+    var num: String = "0"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func btnTapped(_ sender: UIButton) {
-        switch sender.tag {
-        case 0:
-            resultTextView.text = "0"
-        case 1:
-            resultTextView.text = "1"
-        case 2:
-            resultTextView.text = "2"
-        case 3:
-            resultTextView.text = "3"
-        case 4:
-            resultTextView.text = "4"
-        case 5:
-            resultTextView.text = "5"
-        case 6:
-            resultTextView.text = "6"
-        case 7:
-            resultTextView.text = "7"
-        case 8:
-            resultTextView.text = "8"
-        case 9:
-            resultTextView.text = "9"
-        default:
-            break
-        }
+            switch sender.tag {
+            case 0:
+                resultTextView.text = "0"
+            case 1:
+                num += "1"
+                resultTextView.text = num
+            case 2:
+                resultTextView.text = "2"
+            case 3:
+                resultTextView.text = "3"
+            case 4:
+                resultTextView.text = "4"
+            case 5:
+                resultTextView.text = "5"
+            case 6:
+                resultTextView.text = "6"
+            case 7:
+                resultTextView.text = "7"
+            case 8:
+                resultTextView.text = "8"
+            case 9:
+                resultTextView.text = "9"
+            default:
+                break
+            }
+
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
