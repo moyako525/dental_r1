fcd0933d0f52f5c90f595a2ea8c920ea4c702808
jinikuadmin <jinikuadmin@akcpwrxn1zh3qd.local>
2016-12-24 10:09:06 +0900

    足し算できるようになった

--- Dentaku2/ViewController.swift
+++ Dentaku2/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     var intNum2: Int = 0
     var result: Int = 0
     var numArray = [Int]()
+    var activeBtn = false
 //    var count: Int = 0
     
 
@@ -33,48 +34,76 @@ class ViewController: UIViewController {
                 //AC:10 +:11 =:12
             case 0:
                 resultTextView.text = "0"
+                activeBtn = true
             case 1:
                 num = "1"
                 resultTextView.text = num
+                activeBtn = true
             case 2:
                 num = "2"
                 resultTextView.text = num
+                activeBtn = true
             case 3:
                 num = "3"
                 resultTextView.text = num
+                activeBtn = true
             case 4:
                 num = "4"
                 resultTextView.text = num
+                activeBtn = true
             case 5:
                 num = "5"
                 resultTextView.text = num
+                activeBtn = true
             case 6:
                 num = "6"
                 resultTextView.text = num
+                activeBtn = true
             case 7:
                 num = "7"
                 resultTextView.text = num
+                activeBtn = true
             case 8:
                 num = "8"
                 resultTextView.text = num
+                activeBtn = true
             case 9:
                 num = "9"
                 resultTextView.text = num
+                activeBtn = true
             case 10:
                 num = "0"
                 resultTextView.text = num
+                activeBtn = false
                 numArray.removeAll()
             case 11:
-                intNum1 = Int(resultTextView.text)!
-                num = "0"
-                numArray.append(intNum1)
-                result = numArray.reduce(0, +)
-                resultTextView.text = String(result)
+                if activeBtn == true {
+                    intNum1 = Int(resultTextView.text)!
+                    num = "0"
+                    numArray.append(intNum1)
+                    result = numArray.reduce(0, +)
+                    resultTextView.text = String(result)
+                    activeBtn = false
+                }else{
+                    num = "0"
+                    result = numArray.reduce(0, +)
+                    resultTextView.text = String(result)
+                    activeBtn = false
+                }
             case 12:
-                intNum2 = Int(resultTextView.text)!
-                numArray.append(intNum2)
-                result = numArray.reduce(0, +)
-                resultTextView.text = String(result)
+                if activeBtn == true {
+                    intNum2 = Int(resultTextView.text)!
+                    num = "0"
+                    numArray.append(intNum2)
+                    result = numArray.reduce(0, +)
+                    resultTextView.text = String(result)
+                    activeBtn = false
+                }else{
+                    num = "0"
+                    result = numArray.reduce(0, +)
+                    resultTextView.text = String(result)
+                    activeBtn = false
+                }
             default:
                 break
             }
@@ -84,48 +113,76 @@ class ViewController: UIViewController {
             case 0:
                 num += "0"
                 resultTextView.text = num
+                activeBtn = true
             case 1:
                 num += "1"
                 resultTextView.text = num
+                activeBtn = true
             case 2:
                 num += "2"
                 resultTextView.text = num
+                activeBtn = true
             case 3:
                 num += "3"
                 resultTextView.text = num
+                activeBtn = true
             case 4:
                 num += "4"
                 resultTextView.text = num
+                activeBtn = true
             case 5:
                 num += "5"
                 resultTextView.text = num
+                activeBtn = true
             case 6:
                 num += "6"
                 resultTextView.text = num
+                activeBtn = true
             case 7:
                 num += "7"
                 resultTextView.text = num
+                activeBtn = true
             case 8:
                 num += "8"
                 resultTextView.text = num
+                activeBtn = true
             case 9:
                 num += "9"
                 resultTextView.text = num
+                activeBtn = true
             case 10:
                 num = "0"
                 resultTextView.text = num
+                activeBtn = false
                 numArray.removeAll()
             case 11:
-                intNum1 = Int(resultTextView.text)!
-                num = "0"
-                numArray.append(intNum1)
-                result = numArray.reduce(0, +)
-                resultTextView.text = String(result)
+                if activeBtn == true {
+                    intNum1 = Int(resultTextView.text)!
+                    num = "0"
+                    numArray.append(intNum1)
+                    result = numArray.reduce(0, +)
+                    resultTextView.text = String(result)
+                    activeBtn = false
+                }else{
+                    num = "0"
+                    result = numArray.reduce(0, +)
+                    resultTextView.text = String(result)
+                    activeBtn = false
+                }
             case 12:
-                intNum2 = Int(resultTextView.text)!
-                numArray.append(intNum2)
-                result = numArray.reduce(0, +)
-                resultTextView.text = String(result)
+                if activeBtn == true {
+                    intNum2 = Int(resultTextView.text)!
+                    num = "0"
+                    numArray.append(intNum2)
+                    result = numArray.reduce(0, +)
+                    resultTextView.text = String(result)
+                    activeBtn = false
+                }else{
+                    num = "0"
+                    result = numArray.reduce(0, +)
+                    resultTextView.text = String(result)
+                    activeBtn = false
+                }
             default:
                 break
             }
